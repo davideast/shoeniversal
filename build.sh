49738bb6bee736345b5b46f0fc1e77f1a050cd39
David East <deast@google.com>
2017-09-18 05:25:59 -0600

    add build optimizer

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-node_modules/.bin/ng build --prod
-node_modules/.bin/ng build --prod --app 1 --output-hashing none
+node_modules/.bin/ng build --prod --build-optimizer
+node_modules/.bin/ng build --prod --build-optimizer --app 1 --output-hashing none
 tsc -p server/tsconfig.functions.json
 mv static/index.html functions/
 cp src/shoes.json static/
